abc8bbbe96ef697d742f8ee7b5257e7257823536
Divyansh Singh <divyansh222f@gmail.com>
2026-09-04 19:36:49 +0530

    feat: use DS logo as site favicon (all pages)

--- experience/script.js
+++ experience/script.js
@@ -34,10 +34,10 @@ document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
         document.title = "Certificates | Portfolio Divyansh Singh";
-        $("#favicon").attr("href","../assets/images/favhand.png");
+        $("#favicon").attr("href","../assets/images/favicon.png?v=2");
     }
     else {
         document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","../assets/images/favhand.png");
+        $("#favicon").attr("href","../assets/images/favicon.png?v=2");
     }
 });
--- projects/script.js
+++ projects/script.js
@@ -34,11 +34,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Projects | Portfolio Divyansh Singh";
-            $("#favicon").attr("href", "../assets/images/favhand.png");
+            $("#favicon").attr("href", "../assets/images/favicon.png?v=2");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "../assets/images/favhand.png");
+            $("#favicon").attr("href", "../assets/images/favicon.png?v=2");
         }
     });
 
